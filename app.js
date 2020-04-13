f6a7ed7900b6c57c3470b755a89a26c99879807b
Rushil dhinoja <mr.rushil17@gmail.com>
2020-04-13 11:52:03 +0530

    Testing GitIgnore

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+// App.js
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
